1258b704350e9b24695093cf5142fb1c73da0b56
daroyni12 <daroyni12@gmail.com>
2018-11-03 22:43:50 +0700

    Dardyni fadli Zidani/311810685/TI.18.C.2

---
+++ soal2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N,X,T,batas;
+    N=85;
+    batas=N+100;
+    X=20;
+    T=N;
+
+    while (T<=batas)
+    {
+        T=T+X;
+        X=X+10;
+
+    }
+    cout << T;
+    return 0;
+}
